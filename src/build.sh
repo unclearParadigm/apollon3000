0c982750a6f5d060a8fb4ec4ff3bdeb94287d30b
Raffael Rehberger <rarepublic@protonmail.com>
2019-06-24 21:05:14 +0200

    allow cross-platform build to develop easier from desktop

--- src/build.sh
+++ src/build.sh
@@ -1,1 +1,23 @@
-gcc main.c -lwiringPi -lpthread -Wall --pedantic -std=c99 -o apollon3000
+platform=$(eval uname -i)
+outputdirectory="../build/$platform"
+
+echo "=============================================="
+echo "APOLLON 3000 BUILD-SUITE"
+echo "(C) Rehberger Raffael"
+echo "(C) Sarcevic Lejla"
+echo "=============================================="
+echo "Target Platform       :       $platform"
+echo "Output Directory      :       $outputdirectory"
+echo "=============================================="
+
+mkdir -p $outputdirectory
+
+if [ "$platform" = "x86_64" ]; then
+  # Plain Desktop Build
+  gcc main.c -lpthread -Wall --pedantic -std=c99 -o "$outputdirectory/apollon3000"
+fi
+
+if [ "$platform" = "armhf" ] || [ "$platform" = "armv6" ] || [ "$platform" = "armv7" ]; then
+    # Raspberry PI Build
+    gcc main.c -lwiringPi -lpthread -Wall --pedantic -std=c99 -o "$outputdirectory/apollon3000"
+fi
