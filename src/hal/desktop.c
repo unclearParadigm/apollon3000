f5246a0fd200c9463b72d5a5b8cf67886ed4dfee
Raffael Rehberger <rarepublic@protonmail.com>
2019-06-25 06:50:08 +0200

    implement interface for hal interaction

--- src/hal/desktop.c
+++ src/hal/desktop.c
@@ -1,5 +1,24 @@
+#include <stdio.h>
+
+#include "../boolean.h"
 #include "hal.h"
 
 #ifdef BUILD_DESKTOP
+
+void initializeHal(PinConfiguration *pinConfiguration) {
+    printf("Initialize red LED on PIN: %d", pinConfiguration->redPin);
+    printf("Initialize blue LED on PIN: %d", pinConfiguration->bluePin);
+    printf("Initialize green LED on PIN: %d", pinConfiguration->greenPin);
+}
+
+void setPin(int pinNumber, int value) {
+    if(value == DIGITAL_ON) {
+        printf("Set Pin '%d' to ON", pinNumber);
+    } else if(value == DIGITAL_OFF) {
+        printf("Set Pin '%d' to ON", pinNumber);
+    } else  {
+        printf("Set Pin '%d' to value dimmed %d", pinNumber, value);
+    }
+}
 
 #endif
--- src/hal/hal.h
+++ src/hal/hal.h
@@ -1,4 +1,18 @@
-#ifndef CMDARGS_H_
-#define CMDARGS_H_
+#ifndef HAL_H_
+#define HAL_H_
+
+#define DIGITAL_ON  100
+#define DIGITAL_OFF 0
+
+struct PinConfiguration {
+    int redPin;
+    int bluePin;
+    int greenPin;
+};
+
+typedef struct PinConfiguration PinConfiguration;
+
+void initializeHal(PinConfiguration *pinConfiguration);
+void setPin(int pinNumber, int value);
 
 #endif
