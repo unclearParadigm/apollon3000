160c8fc562e72ac2610aa7b5787192bcee84c406
Raffael Rehberger <rarepublic@protonmail.com>
2019-06-25 21:35:04 +0200

    implement raspberryPI HAL

--- src/hal/raspberry.c
+++ src/hal/raspberry.c
@@ -2,4 +2,43 @@
 
 #ifdef BUILD_RASPBERRY
 
+// wiringPi & softPwm are only available on RaspberryPIs
+#include <wiringPi.h>
+#include <softPwm.h>
+
+int REDPINVALUE = DIGITAL_OFF;
+int BLUEPINVALUE = DIGITAL_OFF;
+int GREENPINVALUE = DIGITAL_OFF;
+PinConfiguration *GLOBALPINCONFIG;
+
+void initializeHal(PinConfiguration *pinConfiguration) {
+    GLOBALPINCONFIG = pinConfiguration; /* Set pinConfiguration globally */
+    softPwmCreate(GLOBALPINCONFIG->redPin, 0, 100);
+    softPwmCreate(GLOBALPINCONFIG->bluePin, 0, 100);
+    softPwmCreate(GLOBALPINCONFIG->greenPin, 0, 100);
+}
+
+int getPin(int pinNumber) {
+    if(pinNumber == GLOBALPINCONFIG->redPin) return REDPINVALUE;
+    if(pinNumber == GLOBALPINCONFIG->bluePin) return BLUEPINVALUE;
+    if(pinNumber == GLOBALPINCONFIG->greenPin) return GREENPINVALUE;
+    return -1;
+}
+
+void setPin(int pinNumber, int value) {
+    if(pinNumber == GLOBALPINCONFIG->redPin) REDPINVALUE = value;
+    if(pinNumber == GLOBALPINCONFIG->bluePin) BLUEPINVALUE = value;
+    if(pinNumber == GLOBALPINCONFIG->greenPin) GREENPINVALUE = value;
+
+    softPwmWrite(pinNumber, value);
+}
+
+void togglePin(int pinNumber) {
+    if(getPin(pinNumber) == DIGITAL_ON) {
+        setPin(pinNumber, DIGITAL_OFF);
+    } else {
+        setPin(pinNumber, DIGITAL_ON);
+    }
+}
+
 #endif
