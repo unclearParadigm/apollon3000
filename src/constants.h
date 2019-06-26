78128e4adcf77bd084ed0e981a1e1c6abffb62f1
Raffael Rehberger <rarepublic@protonmail.com>
2019-06-26 23:45:23 +0200

    refactor rcmain a bit

--- src/constants.h
+++ src/constants.h
@@ -5,4 +5,7 @@ const   int     REDPIN     = 0;
 const   int     BLUEPIN    = 23;
 const   int     GREENPIN   = 21;
 
+
+const   int     TCPPORT    = 8080;
+
 #endif
--- src/rcmain.c
+++ src/rcmain.c
@@ -7,66 +7,84 @@
 # include <netinet/in.h>
 # include <arpa/inet.h>
 
-void err_exit(char *message) {
-    perror(message);
-    exit(1);
-}
+#include "boolean.h"
+#include "constants.h"
+#include "hal/hal.h"
+
+
+void startListeningForCommands();
+
 
 int main(int argc, char **argv) {
+    /* Configure PIN-Assignment */
+    PinConfiguration *pinConfig = malloc(sizeof(PinConfiguration));
+    pinConfig->redPin = REDPIN;
+    pinConfig->bluePin = BLUEPIN;
+    pinConfig->greenPin = GREENPIN;
+
+    /* Initialize Hardware Abstraction Layer */
+    initializeHal(pinConfig);
+
+    while(1) {
+        startListeningForCommands();
+        printf("Restart Listening on TCP-Socket");
+    }
+
+    return(0);
+}
+
+
+void startListeningForCommands() {
     fd_set input_fdset;
     int sock_fd, client_fd, length;
     static char buffer[sizeof(int)];
     struct sockaddr_in my_addr, client_addr;
-    socklen_t addr_size = sizeof(struct sockaddr_in);;
-
+    socklen_t addr_size = sizeof(struct sockaddr_in);
     sock_fd = socket(PF_INET, SOCK_STREAM, 0);
+
     if (sock_fd == -1) printf("socket-creation failed");
 
     my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(8080);
+    my_addr.sin_port = htons(TCPPORT);
     my_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(sock_fd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr_in)) == -1)
-        err_exit("bind() failed");
-
-    if (listen(sock_fd, 1) == -1) 
-        err_exit("listen() failed");
-
+    if (bind(sock_fd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr_in)) == -1) {
+        printf("bind() failed");
+        return;
+    }
+    
+    if (listen(sock_fd, 1) == -1)  {
+        printf("listen() failed");
+        return;
+    }
     client_fd = accept(sock_fd, (struct sockaddr *) &client_addr, &addr_size);
-    if (client_fd == -1) 
-        err_exit("accept() failed");
+    if (client_fd == -1) {
+        printf("accept() failed");
+        return;
+    }
 
     printf("Remote-Controller connected from: %s\n", inet_ntoa(client_addr.sin_addr));
 
-    while (1) {
+    while (1) { /* Client connected - start "receive"-loop */
         FD_ZERO(&input_fdset);
-        FD_SET(STDIN_FILENO, &input_fdset);
         FD_SET(client_fd, &input_fdset);
-
-        if (select(client_fd+1, &input_fdset, NULL, NULL, NULL) == -1)
-            err_exit("select() failed");
+        FD_SET(STDIN_FILENO, &input_fdset);
+  
+        if (select(client_fd+1, &input_fdset, NULL, NULL, NULL) == -1) {
+            printf("select() failed");
+            break;
+        }
         
-        if (FD_ISSET(STDIN_FILENO, &input_fdset)) {
-            if (fgets(buffer, 256, stdin) == NULL) {
-                printf("Closing socket.\n");
-                break;
-            }
-
-            length = strlen(buffer);
-            send(client_fd, buffer, length, 0);
+        length = recv(client_fd, buffer, sizeof(int), 0);
+        if (length == 0) {
+            printf("Connection closed by remote host.\n");
+            break;
         }
 
-        else {
-            length = recv(client_fd, buffer, sizeof(int), 0);
-            if (length == 0) {
-                printf("Connection closed by remote host.\n");
-                break;
-            }
-
-            write(STDOUT_FILENO, buffer, length);
-        }
+        int pinValue = atoi(buffer);
+        setPin(REDPIN, pinValue);
     }
+
     close(client_fd);
     close(sock_fd);
-    return(0);
 }
